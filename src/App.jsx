24ab57e2a77fb5c1015ab9ec41be983160a22e06
yousefomar724 <yousefomar724@gmail.com>
2024-06-07 21:13:07 +0300

    clean routing code - generlize theming for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import { Toaster } from "react-hot-toast";
-import {
-  Outlet,
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout";
-
-import Signin from "./pages/auth/signin";
-import Register from "./pages/auth/register";
 
 import Home from "./pages/home";
 import Jobs from "./pages/jobs";
@@ -17,29 +9,19 @@ import Events from "./pages/events";
 import Search from "./pages/search";
 import Groups from "./pages/groups";
 import Fundnig from "./pages/fundnig";
+import Landing from "./pages/landing";
 import Messages from "./pages/messages";
 import Business from "./pages/business";
-import Landing from "./pages/landing";
-import { ThemeProvider } from "./components/ThemeProvider";
 
 export default function App() {
   return (
     <div className="font-rubik">
       <Router>
         <Routes>
-          <Route
-            element={
-              <ThemeProvider>
-                <Outlet />
-              </ThemeProvider>
-            }
-          >
-            <Route path="/" element={<Landing />} />
-            {/* <Route path="/signin" element={<Signin />} /> */}
-            {/* <Route path="/register" element={<Register />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} /> */}
-          </Route>
+          {/* Landing page (Login) */}
+          <Route path="/" element={<Landing />} />
+
+          {/* Dashboard */}
           <Route element={<Layout />}>
             <Route path="/home" element={<Home />} />
             <Route path="/jobs" element={<Jobs />} />
@@ -49,11 +31,6 @@ export default function App() {
             <Route path="/funding" element={<Fundnig />} />
             <Route path="/messages" element={<Messages />} />
             <Route path="/business" element={<Business />} />
-
-            {/* Protected Routes */}
-            {/* <Route element={<RequireAuth allowedRoles={["User"]} />}>
-              <Route path="/profile" element={<Profile />} />
-            </Route> */}
           </Route>
         </Routes>
       </Router>
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "./components/ThemeProvider.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.jsx";
@@ -9,6 +10,8 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </QueryClientProvider>
 );
